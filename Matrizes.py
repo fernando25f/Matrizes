5b7c88d822ab707795394ed03dc827734005a080
f3rnandof1lho <fernandofilho.engwatt@gmail.com>
2022-04-26 14:55:35 -0300

    Update Matrizes.py

--- Matrizes.py
+++ Matrizes.py
@@ -6,15 +6,22 @@ class Matriz():
     def __init__(self, linhas, colunas, matriz=list()):
         self.linhas = linhas
         self.colunas = colunas
-        
+
         #Criando a Matriz
         if matriz == list():
             matriz = list()
             for i in range(0, self.linhas):
                 linha = list()
                 for j in range(0, self.colunas):
-                    item = int(input("Digite o valor do item [{i},{j}]: "))
+                    item = int(input(f"Digite o valor do item [{i+1},{j+1}]: "))
                     linha.append(item)
                 matriz.append(linha)
         self.matriz = matriz
+
+    # Exibir a Matriz
+    def Mostrar(self):
+        print("Sua matriz é: \n")
+        for l in range(0, self.linhas):
+            print(self.matriz[l])
+
 
